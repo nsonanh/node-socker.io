5f03024d8470cc2f7b3376985591aad335bde221
Son Nguyen <son.nguyen@fdssoft.com>
2018-12-14 17:16:10 +0700

    Initial setup

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+server = app.listen(3000);
+
+
+const io = require('socket.io')(server)
+
+io.on('connection', (socket) => {
+	console.log('New user connected');
+
+	//default username
+	socket.username = "Anonymous";
+
+  //listen on change_username
+  socket.on('change_username', (data) => {
+      socket.username = data.username
+  });
+
+  //listen on new_message
+  socket.on('new_message', (data) => {
+      //broadcast the new message
+      io.sockets.emit('new_message', {message : data.message, username : socket.username});
+  });
+
+  //listen on typing
+  socket.on('typing', (data) => {
+  	socket.broadcast.emit('typing', {username : socket.username});
+  });
+});
